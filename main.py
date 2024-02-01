b3eaf5198139ff07a84ce41ae6ac4a6b77c310ab
tashlykova <20elenatashlykova08@gmail.com>
2024-02-01 18:36:32 +0500

    pgUp and pgDown

--- main.py
+++ main.py
@@ -6,22 +6,6 @@ import pygame
 from api_library import get_static
 
 
-# def load_image(name, colorkey=None):
-#     fullname = os.path.join('data', name)
-#     if not os.path.isfile(fullname):
-#         print(f"Файл с изображением '{fullname}' не найден")
-#         sys.exit()
-#     image = pygame.image.load(fullname)
-#     if colorkey is not None:
-#         image = image.convert()
-#         if colorkey == -1:
-#             colorkey = image.get_at((0, 0))
-#         image.set_colorkey(colorkey)
-#     else:
-#         image = image.convert_alpha()
-#     return image
-
-
 class BigMap:
     def __init__(self):
         self.image = None
@@ -41,7 +25,12 @@ class BigMap:
         self.image = pygame.image.load(image)
 
     def event_hendler(self, event):
-        pass
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_PAGEUP:
+                self.z = min(self.z + 1, 21)
+            if event.key == pygame.K_PAGEDOWN:
+                self.z = max(self.z - 1, 0)
+            self.update_map()
 
     def draw(self, surf):
         surf.blit(self.image, (0, 0))
